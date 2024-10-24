dbb7cd9fe31dec360d61303034cd664090f71d7e
Nathanael Wong <natgeo.wong@outlook.com>
2024-10-24 14:33:13 -0400

    Forgot to set top boundary conditions lol

--- wtg_jas2008.f90
+++ wtg_jas2008.f90
@@ -26,8 +26,8 @@ implicit none
       w_wtg(k) = 0
    end do
 
-   tv_lsbg = tg0   * (1+0.61*qg0)
-   tv_wave = tabs0 * (1+0.61*qv0)
+   tv_lsbg = tg0   * (1. +0.61*qg0)
+   tv_wave = tabs0 * (1. +0.61*qv0)
    dwwtgdt = 0.
 
    call calc_wtend(0.5*pi/lambda_wtg, w_wtg(1:ktrop), tv_wave(1:ktrop), &
@@ -42,86 +42,86 @@ implicit none
                         z_full, z_half, wtend, nz)
    !     ------------------------------ input arguments ------------------------------
 
-         integer, intent(in) :: nz  ! number of midpoint levels, the number of interface levels is nz+1
+      integer, intent(in) :: nz  ! number of midpoint levels, the number of interface levels is nz+1
 
-         real, dimension(nz), intent(in) ::                 &
-         tv_curr,           &       ! Cell center virtual temperature
-         w_curr,            &       ! Cell center wave vertical velocity
-         tv_fullbg,         &       ! Cell center background virtual temperature
-         z_full,            &       ! Cell center height
-         rho_full                   ! Cell center density
-         real, dimension(nz+1), intent(in) ::                 &
-         z_half                     ! Interface-level height, including the surface, which is at height zero
+      real, dimension(nz), intent(in) ::                 &
+      tv_curr,           &       ! Cell center virtual temperature
+      w_curr,            &       ! Cell center wave vertical velocity
+      tv_fullbg,         &       ! Cell center background virtual temperature
+      z_full,            &       ! Cell center height
+      rho_full                   ! Cell center density
+      real, dimension(nz+1), intent(in) ::                 &
+      z_half                     ! Interface-level height, including the surface, which is at height zero
 
-         real, intent (in) :: wn    !the specified horizontal wavenumber
+      real, intent (in) :: wn    !the specified horizontal wavenumber
 
-   !     ------------------------------ output arguments -----------------------------
+!     ------------------------------ output arguments -----------------------------
 
-         real, dimension(nz), intent(out) ::                 &
-         wtend                      ! Cell center w tendency
+      real, dimension(nz), intent(out) ::                 &
+      wtend                      ! Cell center w tendency
 
-   !     ------------------------------ local variables ------------------------------
-         real  N2top                ! Top-level buoyancy frequency squared
+!     ------------------------------ local variables ------------------------------
+      real  N2top                ! Top-level buoyancy frequency squared
 
-         real, dimension(1:nz+1) ::                                             &
-         dz                         ! grid spacing between midpoint levels
-         real, dimension(1:nz) ::                                             &
-         rhs,               &       ! The right hand side for the Gauss Elimination
-         aa,                &       ! aa, bb, cc are the three columns of the tridiagonal matrix
-         bb,                &
-         cc
+      real, dimension(1:nz+1) ::                                             &
+      dz                         ! grid spacing between midpoint levels
+      real, dimension(1:nz) ::                                             &
+      rhs,               &       ! The right hand side for the Gauss Elimination
+      aa,                &       ! aa, bb, cc are the three columns of the tridiagonal matrix
+      bb,                &
+      cc
 
-         integer :: k               ! Vertical loop counter
+      integer :: k               ! Vertical loop counter
 
-   !     ------------------------------ executable code ------------------------------
-      
-         N2top=ggr/tv_fullbg(nz)*((tv_fullbg(nz)-tv_fullbg(nz-1))/(z_full(nz)-z_full(nz-1))+ggr/cp)
+!     ------------------------------ executable code ------------------------------
+   
+      N2top=ggr/tv_fullbg(nz)*((tv_fullbg(nz)-tv_fullbg(nz-1))/(z_full(nz)-z_full(nz-1))+ggr/cp)
 
-   !     compute grid spacing between midpoint levels
-         do k=2,nz
-            dz(k)=z_full(k)-z_full(k-1)
-         end do
-         dz(1)=2*z_full(1)
-         dz(nz+1)=2*(z_half(nz+1)-z_full(nz))
+!     compute grid spacing between midpoint levels
+      do k=2,nz
+         dz(k)=z_full(k)-z_full(k-1)
+      end do
+      dz(1)=z_full(1)
+      dz(nz+1)=2*(z_half(nz+1)-z_full(nz))
 
-   !     Gaussian Elimination
-         rhs=0.
-         do k=2,nz-1
-            rhs(k)=-rho_full(k)*ggr*wn*wn*(tv_curr(k)-tv_fullbg(k))/tv_fullbg(k)*dz(k)*dz(k+1)/2.
-         end do
+!     Gaussian Elimination
+      rhs=0.
+      do k=2,nz-1
+         rhs(k)=-rho_full(k)*ggr*wn*wn*(tv_curr(k)-tv_fullbg(k))/tv_fullbg(k)*dz(k)*dz(k+1)/2.
+      end do
 
-   !     set up the tridiagonal matrix
-         do k=2,nz-1
-            aa(k)=dz(k+1)/(dz(k)+dz(k+1))
-            bb(k)=-1.
-            cc(k)=dz(k)/(dz(k)+dz(k+1))
-         end do
+!     set up the tridiagonal matrix
+      do k=2,nz-1
+         aa(k)=dz(k+1)/(dz(k)+dz(k+1))
+         bb(k)=-1.
+         cc(k)=dz(k)/(dz(k)+dz(k+1))
+      end do
 
-         ! boundary condition at surface, omega'=0 (homogeneous Dirichlet BC).
-         aa(1)=0.
-         bb(1)=1.
-         cc(1)=0.
+      ! boundary condition at surface, omega'=0 (homogeneous Dirichlet BC).
+      aa(1)=0.
+      bb(1)=1.
+      cc(1)=0.
 
-         ! apply omega'=0 at tropopause (homogeneous Dirichlet BC).
-         aa(1)=0.
-         bb(1)=1.
-         cc(1)=0.
+      ! apply omega'=0 at tropopause (homogeneous Dirichlet BC).
+      aa(nz)=0.
+      bb(nz)=1.
+      cc(nz)=0.
 
-         !Gaussian Elimination with no pivoting
-         do k=1,nz-1
-            bb(k+1)=bb(k+1)-aa(k+1)/bb(k)*cc(k)
-            rhs(k+1)=rhs(k+1)-aa(k+1)/bb(k)*rhs(k)
-         end do
+      !Gaussian Elimination with no pivoting
+      do k=1,nz-1
+         bb(k+1)=bb(k+1)-aa(k+1)/bb(k)*cc(k)
+         rhs(k+1)=rhs(k+1)-aa(k+1)/bb(k)*rhs(k)
+      end do
 
-         !backward substitution
-         rhs(nz)=rhs(nz)/bb(nz)
-         do k= nz-1,1,-1
-            rhs(k)=(rhs(k)-cc(k)*rhs(k+1))/bb(k)
-         end do
+      !backward substitution
+      rhs(nz)=rhs(nz)/bb(nz)
+      do k= nz-1,1,-1
+         rhs(k)=(rhs(k)-cc(k)*rhs(k+1))/bb(k)
+      end do
 
-         do k=1,nz
-            wtend(k)=rhs(k)/rho_full(k)
-         end do
+      do k=1,nz
+         wtend(k)=rhs(k)/rho_full(k)
+      end do
 
    end subroutine calc_wtend
 
